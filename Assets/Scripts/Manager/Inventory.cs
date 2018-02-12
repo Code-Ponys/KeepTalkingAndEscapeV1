cd5398e569b3e1119f83c37409686725850c430c
CodeFoxByChojo <playhadde@gmail.com>
2018-02-12 14:13:13 +0100

    added Method to write the input arrays in one array

--- Assets/Scripts/Manager/Inventory.cs
+++ Assets/Scripts/Manager/Inventory.cs
@@ -5,3 +5,11 @@
         [SerializeField] private Image _emptyItemSlot;
         [SerializeField] private Image _ghostSelectorOutline;
         [SerializeField] private Image _humanSelectorOutline;
+        private void CreateSlotsArray() {
+            for(int j = 0; j < _line1.Length-1; j++) {
+                _slots[0, j] = _line1[j];
+                _slots[1, j] = _line2[j];
+                _slots[2, j] = _line3[j];
+                _slots[3, j] = _line4[j];
+            }
+        }
