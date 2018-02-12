dee78b09fa4dd70fef888bc96505b3acf53c044e
CodeFoxByChojo <playhadde@gmail.com>
2018-02-12 14:12:40 +0100

    added input fields

---
+++ Assets/Scripts/Manager/Inventory.cs
@@ -0,0 +1,7 @@
+        [SerializeField] private GameObject[] _line1 = new GameObject[5];
+        [SerializeField] private GameObject[] _line2 = new GameObject[5];
+        [SerializeField] private GameObject[] _line3 = new GameObject[5];
+        [SerializeField] private GameObject[] _line4 = new GameObject[5];
+        [SerializeField] private Image _emptyItemSlot;
+        [SerializeField] private Image _ghostSelectorOutline;
+        [SerializeField] private Image _humanSelectorOutline;
