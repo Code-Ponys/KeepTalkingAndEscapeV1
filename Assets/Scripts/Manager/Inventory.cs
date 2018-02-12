206b808d21f5b22fd80e3b4f07d747c7c8789bb4
CodeFoxByChojo <playhadde@gmail.com>
2018-02-12 18:30:35 +0100

    Added Input function for Inventory UI

--- Assets/Scripts/Manager/Inventory.cs
+++ Assets/Scripts/Manager/Inventory.cs
@@ -73,15 +73,85 @@ namespace TrustfallGames.KeepTalkingAndEscape.Manager {
             }
         }
 
+        /// <summary>
+        /// The input function for the ui manager.
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        private void InventoryInput() {
+            //Button input for human
             if(_characterType == CharacterType.Human) {
-                if(Input.GetAxis(ButtonNames.GhostVerticalPad) < 0) {
+                //Change current choosed Item
+                if(_currentAxisDelay < 0) {
+                    if(Input.GetAxis(ButtonNames.HumanHorizontalPad) < 0) {
+                        //Left
+                        if(_x == 0) return;
+                        _x--;
+                        _currentAxisDelay = _axisDelay;
+                    }
+                    else if(Input.GetAxis(ButtonNames.HumanHorizontalPad) > 0) {
+                        //Right
+                        if(_x == 4) return;
+                        _x++;
+                        _currentAxisDelay = _axisDelay;
+                    }
+
+                    if(Input.GetAxis(ButtonNames.HumanVerticalPad) < 0) {
+                        //Down
+                        if(_y == 3) return;
+                        _y++;
+                        _currentAxisDelay = _axisDelay;
+                    }
+                    else if(Input.GetAxis(ButtonNames.HumanVerticalPad) > 0) {
+                        //Up
+                        if(_y == 0) return;
+                        _y--;
+                        _currentAxisDelay = _axisDelay;
+                    }
+                }
+                else {
+                    _currentAxisDelay -= Time.deltaTime;
                 }
             }
 
+            //Button input for ghost
             else if(_characterType == CharacterType.Ghost) {
-            }
-            else {
-                throw new ArgumentException("Character type not set.");
+                //Change current choosed Item
+                if(_currentAxisDelay <= 0) {
+                    Debug.Log("Input: " + Input.GetAxis(ButtonNames.MoveGhostY));
+                    Debug.Log("Input: " + Input.GetAxis(ButtonNames.MoveGhostX));
+                    if(Input.GetAxis(ButtonNames.MoveGhostX) < 0) {
+                        //Left
+                        Debug.Log("Pressed Left");
+                        if(_x == 0) return;
+                        _x--;
+                        _currentAxisDelay = _axisDelay;
+                    }
+                    if(Input.GetAxis(ButtonNames.MoveGhostX) > 0) {
+                        //Right
+                        Debug.Log("Pressed right");
+                        if(_x == 4) return;
+                        _x++;
+                        _currentAxisDelay = _axisDelay;
+                    }
+
+                    if(Input.GetAxis(ButtonNames.MoveGhostY) < 0) {
+                        //Down
+                        Debug.Log("Pressed Down");
+                        if(_y == 3) return;
+                        _y++;
+                        _currentAxisDelay = _axisDelay;
+                    }
+                    if(Input.GetAxis(ButtonNames.MoveGhostY) > 0) {
+                        //Up
+                        Debug.Log("Pressed Up");
+                        if(_y == 0) return;
+                        _y--;
+                        _currentAxisDelay = _axisDelay;
+                    }
+                }
+                else {
+                    _currentAxisDelay -= Time.deltaTime;
+                }
             }
         }
     }
