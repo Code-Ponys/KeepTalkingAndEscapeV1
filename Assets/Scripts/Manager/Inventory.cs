b683c0dea6099ab716cc821f4953dbe3caa76bf4
CodeFoxByChojo <playhadde@gmail.com>
2018-02-12 18:28:59 +0100

    removed old deprecated method

--- Assets/Scripts/Manager/Inventory.cs
+++ Assets/Scripts/Manager/Inventory.cs
@@ -30,21 +30,9 @@ namespace TrustfallGames.KeepTalkingAndEscape.Manager {
             ValidateData();
         }
 
-        private void CreateSlotsArray() {
-            for(int j = 0; j < _line1.Length - 1; j++) {
-                _slots[0, j] = _line1[j];
-                _slots[1, j] = _line2[j];
-                _slots[2, j] = _line3[j];
-                _slots[3, j] = _line4[j];
-            }
-        }
 
-        private void ValidateData() {
-            if(_line1.Length == _line2.Length && _line1.Length == _line3.Length && _line1.Length == _line4.Length) {
-                CreateSlotsArray();
             }
             else {
-                throw new ArgumentException("Line Arrays must have the same length");
             }
         }
 
