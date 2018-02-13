8e985792a9566dd2d8b4e5b03765a43394ee7855
CodeFoxByChojo <playhadde@gmail.com>
2018-02-13 15:33:14 +0100

    added button images

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -42,6 +42,25 @@ namespace TrustfallGames.KeepTalkingAndEscape.Manager {
         [SerializeField] private Text _ghostFlavourText;
         [SerializeField] private Text _humanFlavourText;
         [SerializeField] private Text _healthText;
+
+        [SerializeField] private Inventory _inventoryGhost;
+        [SerializeField] private Inventory _inventoryHuman;
+        
+        [SerializeField] private Sprite _a;
+        [SerializeField] private Sprite _b;
+        [SerializeField] private Sprite _x;
+        [SerializeField] private Sprite _y;
+
+        [SerializeField] private Sprite _start;
+        [SerializeField] private Sprite _back;
+
+        [SerializeField] private Sprite _rb;
+        [SerializeField] private Sprite _lb;
+        [SerializeField] private Sprite _rt;
+        [SerializeField] private Sprite _lt;
+        [SerializeField] private Sprite _pad;
+        [SerializeField] private Sprite _l;
+        [SerializeField] private Sprite _r;
         private float _timerHuman;
         private float _timerGhost;
         [Range(1,100)][SerializeField] private float _flavourTextWaitTimer;
