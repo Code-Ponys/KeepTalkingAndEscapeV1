c9aaa89c4ff91d8b5705c49430d991a52febb3a0
CodeFoxByChojo <playhadde@gmail.com>
2018-02-13 15:30:57 +0100

    added method to search gameobjects for inventory

--- Assets/Scripts/Manager/Inventory.cs
+++ Assets/Scripts/Manager/Inventory.cs
@@ -30,28 +30,31 @@ namespace TrustfallGames.KeepTalkingAndEscape.Manager {
         }
 
         private void SearchInventoryObjects() {
-            if(_characterType == CharacterType.Ghost) {
-                for(int i = 0; i < 4; i++) {
-                    for(int j = 0; j < 5; j++) {
-                        _slots[i, j] = GameObject.Find(i + "," + j + "G");
-                        _selectorHuman[i, j] = GameObject.Find(i + "," + j + "GSH");
-                        _selectorGhost[i, j] = GameObject.Find(i + "," + j + "GSG");
+            switch(_characterType) {
+                case CharacterType.Ghost:
+                    for(var i = 0; i < 4; i++) {
+                        for(var j = 0; j < 5; j++) {
+                            _slots[i, j] = GameObject.Find(i + "," + j + "G");
+                            _selectorHuman[i, j] = GameObject.Find(i + "," + j + "GSH");
+                            _selectorGhost[i, j] = GameObject.Find(i + "," + j + "GSG");
+                        }
                     }
-                }
 
-                Debug.Log("_slots: " + _slots.Length + " | selector Human: " + _selectorHuman.Length + " | selector Ghost: " + _selectorGhost.Length);
-            }
-            else if(_characterType == CharacterType.Human) {
-                for(int i = 0; i < 4; i++) {
-                    for(int j = 0; j < 5; j++) {
-                        _slots[i, j] = GameObject.Find(i + "," + j + "H");
-                        _selectorHuman[i, j] = GameObject.Find(i + "," + j + "HSH");
-                        _selectorGhost[i, j] = GameObject.Find(i + "," + j + "HSG");
+                    Debug.Log("_slots: " + _slots.Length + " | selector Human: " + _selectorHuman.Length + " | selector Ghost: " + _selectorGhost.Length);
+                    break;
+                case CharacterType.Human:
+                    for(var i = 0; i < 4; i++) {
+                        for(var j = 0; j < 5; j++) {
+                            _slots[i, j] = GameObject.Find(i + "," + j + "H");
+                            _selectorHuman[i, j] = GameObject.Find(i + "," + j + "HSH");
+                            _selectorGhost[i, j] = GameObject.Find(i + "," + j + "HSG");
+                        }
                     }
-                }
-            }
-            else {
-                throw new ArgumentException("Character type not set.");
+
+                    Debug.Log("_slots: " + _slots.Length + " | selector Human: " + _selectorHuman.Length + " | selector Ghost: " + _selectorGhost.Length);
+                    break;
+                default:
+                    throw new Exception("Character type not set.");
             }
         }
 
