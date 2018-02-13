9bb67ab9bf44a228da0ef4fc248e9ee4444f4b26
Nekro9618 <bill9618halim@gmail.com>
2018-02-13 18:01:01 +0100

    Main Menu Start

---
+++ Assets/Scripts/Main Menu/StartGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TrustfallGames.KeepTalkingAndEscape.Manager {
+	public class StartGame : MonoBehaviour {
+
+		private bool HumanPressedButton = false;
+		private bool GhostPressedButton = false;
+
+		[SerializeField] private CharacterType _characterType;
+
+		// Use this for initialization
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+
+		private void Play() {
+			switch(_characterType) {
+				case CharacterType.Human:
+					if(Input.GetButton(ButtonNames.HumanInspect) && HumanPressedButton != true) {
+						HumanPressedButton = true;
+						Debug.Log("Is Human Button pressed: " + HumanPressedButton);
+					}
+					else if(Input.GetButton(ButtonNames.HumanInspect) && HumanPressedButton) {
+						HumanPressedButton = false;
+						Debug.Log("Is Human Button pressed: " + HumanPressedButton);
+					}
+
+					break;
+
+				case CharacterType.Ghost:
+					if(Input.GetButton(ButtonNames.GhostInspect) && GhostPressedButton != true) {
+						GhostPressedButton = true;
+						Debug.Log("Is Ghost Button pressed: " + GhostPressedButton);
+					}
+					else if(Input.GetButton(ButtonNames.GhostInspect) && GhostPressedButton) {
+						GhostPressedButton = false;
+						Debug.Log("Is Ghost Button pressed: " + GhostPressedButton);
+					}
+
+					break;
+			}
+		}
+	}
+}
