d2385af9fbba163f0c52450f915777488b0fae4c
CodeFoxByChojo <playhadde@gmail.com>
2018-02-13 15:28:12 +0100

    added ValidateDatabaseData(). Checks if everythink is valid.

--- Assets/Scripts/DataController/ItemDatabaseHandler.cs
+++ Assets/Scripts/DataController/ItemDatabaseHandler.cs
@@ -23,3 +23,42 @@ namespace TrustfallGames.KeepTalkingAndEscape.DataController {
             ValidateDatabaseData(database);
             return database;
         }
+        private static void ValidateDatabaseData(ItemDatabase database) {
+            foreach(var testitem in database.ItemDatabaseList) {
+                var combinePartnerValid = false;
+                var combineItemValid = false;
+                var count = 0;
+                foreach(var checkitem in database.ItemDatabaseList) {
+                    if(string.Equals(testitem.ItemId, checkitem.ItemId,
+                                     StringComparison.CurrentCultureIgnoreCase)) {
+                        count++;
+                    }
+
+                    if(testitem.CombineWithItem.ToLower() != "none") {
+                        if(string.Equals(checkitem.CombineWithItem, testitem.ItemId,
+                                         StringComparison.CurrentCultureIgnoreCase)) {
+                            if(string.Equals(testitem.CombineWithItem, checkitem.ItemId,
+                                             StringComparison.CurrentCultureIgnoreCase)) {
+                                combinePartnerValid = true;
+                            }
+                        }
+
+                        if(string.Equals(testitem.NextItem, checkitem.ItemId, StringComparison.CurrentCultureIgnoreCase)) {
+                            combineItemValid = true;
+                        }
+                    }
+                }
+
+                if(combinePartnerValid == false) {
+                    throw new Exception("The Item to combine with is not existent.Exception Occured with Item ID: " + testitem.ItemId);
+                }
+
+                if(combineItemValid == false) {
+                    throw new Exception("The combined item id does not exist. Exception Occured with Item ID: " + testitem.ItemId);
+                }
+
+                if(count != 1) {
+                    throw new Exception("The itemId " + testitem.ItemId + " is not unique. Please Check your itemDatabase.yml");
+                }
+            }
+        }
