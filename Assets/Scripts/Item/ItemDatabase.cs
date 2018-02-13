1d2bf813af00b60b7ff37b992bf0891cf5285bbb
CodeFoxByChojo <playhadde@gmail.com>
2018-02-13 17:34:11 +0100

    added test generation fort database

--- Assets/Scripts/Item/ItemDatabase.cs
+++ Assets/Scripts/Item/ItemDatabase.cs
@@ -10,6 +10,25 @@ namespace TrustfallGames.KeepTalkingAndEscape.Datatypes {
         private ItemDatabase() {
         }
 
+        private void Awake() {
+            _itemDatabase = new List<Item>();
+            for(var i = 1; i < 11; i++) {
+                var obj = new Item();
+                obj.ItemId = "testitem" + i;
+                obj.Name = "testitem " + i;
+                obj.SpritePath = "assets/2D_Art/itempics/sendnudes.jpg";
+                obj.GhostDescription = "testitem" + i + " beschreibung geist";
+                obj.HumanDescription = "testitem" + i + " beschreibung human";
+                obj.Combineable = true;
+                obj.CombineWith = CombineWith.Item;
+                obj.CombineWithItem = "testitem " + i + 1;
+                obj.NextItem = "testitem " + i + 2;
+                _itemDatabase.Add(obj);
+            }
+            ItemDatabaseHandler.WriteDatabase();
+        }
+
+
         public static ItemDatabase GetInstance() {
             return GameObject.Find("ItemDatabase").GetComponent<ItemDatabase>();
         }
