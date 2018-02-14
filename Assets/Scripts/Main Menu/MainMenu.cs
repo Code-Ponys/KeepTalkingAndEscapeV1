e855fd9a6922a92a521b1a8f7a7892d5f4e439be
Nekro9618 <bill9618halim@gmail.com>
2018-02-14 19:37:56 +0100

    Main Menu only for New Game

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,73 +1,199 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace TrustfallGames.KeepTalkingAndEscape.Manager {
     public class MainMenu : MonoBehaviour {
 
+        [SerializeField] private GameObject[] _menu;
+
         [SerializeField] private float _axisDelay = 0.5f;
         [SerializeField] private CharacterType _characterType;
+
         private float _currentAxisDelay;
-        private int _x = 0;
+        private static int _currentPressedButtonHuman;
+        private static int _currentPressedButtonGhost;
+        private ControllerChecker _controllerChecker;
+        
+        [SerializeField] private Sprite _outline;
+        [SerializeField] private Sprite _defaultSprite;
+        
         private int _y = 0;
 
-        // Use this for initialization
-        void Start() {
+        private void Start() {
+            _currentPressedButtonHuman = 5;
+            _currentPressedButtonGhost = 5;    
         }
 
         // Update is called once per frame
         void Update() {
+            MainMenuInput();
+            UpdateSelection();
+            Clicked();
+            SceneManagement();
+        }
+        
+        private void UpdateSelection() {
+            foreach(var obj in _menu) {
+                obj.GetComponent<Image>().sprite = _defaultSprite;
+            }
+
+            foreach(var obj in _menu) {
+                obj.GetComponent<Image>().sprite = _defaultSprite;
+            }
+            switch(_characterType) {
+                case CharacterType.Ghost:
+                    _menu[_y].GetComponent<Image>().sprite = _outline;
+                    break;
+                case CharacterType.Human:
+                    _menu[_y].GetComponent<Image>().sprite = _outline;
+                    break;
+            }
         }
 
-        private void InventoryInput() {
+        private void MainMenuInput() {
             //Button input for human
             switch(_characterType) {
                 case CharacterType.Human:
                     //Change current button
                     if(_currentAxisDelay < 0) {
-                        if(Input.GetAxis(ButtonNames.MoveHumanX) < 0) {
-                            //Left
-                            if(_x == 0) return;
-                            _x--;
+                        if(Input.GetAxis(ButtonNames.MoveHumanY) < 0) {
+                            //Up
+                            if(_y == 4) return;
+                            _y++;
                             _currentAxisDelay = _axisDelay;
                         }
-                        else if(Input.GetAxis(ButtonNames.MoveHumanX) > 0) {
-                            //Right
-                            if(_x == 4) return;
-                            _x++;
+                        else if(Input.GetAxis(ButtonNames.MoveHumanY) > 0) {
+                            //Down
+                            if(_y == 0) return;
+                            _y--;
                             _currentAxisDelay = _axisDelay;
                         }
                     }
                     else {
                         _currentAxisDelay -= Time.deltaTime;
                     }
-
                     break;
                 case CharacterType.Ghost:
                     //Change current button
-                    if(_currentAxisDelay <= 0) {
-                        if(Input.GetAxis(ButtonNames.MoveGhostX) < 0) {
-                            //Left
-                            Debug.Log("Pressed Left");
-                            if(_x == 0) return;
-                            _x--;
+                    if(_currentAxisDelay < 0) {
+                        if(Input.GetAxis(ButtonNames.MoveGhostY) < 0) {
+                            //Up
+                            Debug.Log("Pressed Down");
+                            if(_y == 4) return;
+                            _y++;
                             _currentAxisDelay = _axisDelay;
                         }
 
-                        if(Input.GetAxis(ButtonNames.MoveGhostX) > 0) {
-                            //Right
-                            Debug.Log("Pressed right");
-                            if(_x == 4) return;
-                            _x++;
+                        if(Input.GetAxis(ButtonNames.MoveGhostY) > 0) {
+                            //Down
+                            Debug.Log("Pressed Up");
+                            if(_y == 0) return;
+                            _y--;
                             _currentAxisDelay = _axisDelay;
                         }
                     }
                     else {
                         _currentAxisDelay -= Time.deltaTime;
                     }
-
                     break;
             }
+        }
+        
+        public void Clicked() {
+            switch(_characterType) {
+                case CharacterType.Human:
+                    if(_currentAxisDelay <= 0) {
+                        if(Input.GetButtonDown(ButtonNames.HumanInspect)) {
+                            if(_menu[_y] != null) {
+                                if(_menu[0]) {
+                                    _currentPressedButtonHuman =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                    Debug.Log("Pressed Human Button is number: " + _currentPressedButtonHuman + _currentPressedButtonGhost);
+                                }
+                                
+                                if(_menu[1]) {
+                                    _currentPressedButtonHuman =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+                                
+                                if(_menu[2]) {
+                                    _currentPressedButtonHuman =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+                                
+                                if(_menu[3]) {
+                                    _currentPressedButtonHuman =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+                                
+                                if(_menu[4]) {
+                                    _currentPressedButtonHuman =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        _currentAxisDelay -= Time.deltaTime;
+                    }
+                    break;
+
+                case CharacterType.Ghost:
+                    if(_currentAxisDelay < 0) {
+                       if(Input.GetButtonDown(ButtonNames.GhostInspect)) {
+                            if(_menu[_y] != null) {
+                                if(_menu[0]) {
+                                    _currentPressedButtonGhost =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+                                
+                                if(_menu[1]) {
+                                    _currentPressedButtonGhost =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+                                
+                                if(_menu[2]) {
+                                    _currentPressedButtonGhost =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+                                
+                                if(_menu[3]) {
+                                    _currentPressedButtonGhost =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+
+                                
+                                if(_menu[4]) {
+                                    _currentPressedButtonGhost =_y;
+                                    _currentAxisDelay = _axisDelay;
+                                }
+
+                                Debug.Log("Pressed Ghost Button is number: " + _currentPressedButtonGhost);
+                            }
+                        }
+                    }
+                    else {
+                        _currentAxisDelay -= Time.deltaTime;
+                    }
+                    break;
+            }
+        }
+
+        private void SceneManagement() {
+            if(_currentPressedButtonHuman == 0 && _currentPressedButtonGhost == 0) {
+                SceneManager.LoadScene("V10_Wohnung_Bill");
+            }
+        }
+        
+        private CharacterType CharacterType {
+            get {return _characterType;}
+        }
+
+        private int Y {
+            get {return _y;}
         }
     }
 }
