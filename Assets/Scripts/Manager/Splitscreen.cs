acb18b80c008536b6340449c3b5dc020cef9db8e
Nekro9618 <bill9618halim@gmail.com>
2018-02-21 11:19:28 +0100

    Test Build

---
+++ Assets/Scripts/Manager/Splitscreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splitscreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(5760,1440,false);
+	}
+
+}
