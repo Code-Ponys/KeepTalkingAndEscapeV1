b6819942980bf2e96908950446ff8e45b85c9f79
CodeFoxByChojo <playhadde@gmail.com>
2018-02-21 19:47:34 +0100

    added Input Function

--- Assets/Scripts/ObjectHandler/MapHandler.cs
+++ Assets/Scripts/ObjectHandler/MapHandler.cs
@@ -3,3 +3,52 @@
             _buttons.Add(button);
             _gameManager = GameManager.GetGameManager();
         }
+
+
+        public void InputMap() {
+            if(_currentAxisDelay < 0) {
+                if(Input.GetAxis(ButtonNames.MoveHumanX) < 0) {
+                    //Left
+                    ChangeActiveButton(Direction.Left);
+                }
+                else if(Input.GetAxis(ButtonNames.MoveHumanX) > 0) {
+                    //Right
+                    ChangeActiveButton(Direction.Right);
+                }
+
+                if(Input.GetAxis(ButtonNames.MoveHumanY) < 0) {
+                    //Down
+                    ChangeActiveButton(Direction.Down);
+                }
+                else if(Input.GetAxis(ButtonNames.MoveHumanY) > 0) {
+                    //Up
+                    ChangeActiveButton(Direction.Up);
+                }
+            }
+            else {
+                _currentAxisDelay -= Time.deltaTime;
+            }
+
+            if(Input.GetButtonDown(ButtonNames.HumanInspect)) {
+                _passwordText = _passwordText + _currentActiveButton.ButtonNumber;
+                if(_passwordText.Length > 0 && _password[0] == _passwordText[0]) {
+                    if(_passwordText.Length > 1 && _password[1] == _passwordText[1]) {
+                        if(_passwordText.Length > 2 &&_password[2] == _passwordText[2]) {
+                        }
+                        else {
+                            _passwordText = "";
+                            _gameManager.HumanController.TakeHealth(1);
+                        }
+                    }
+                    else {
+                        _passwordText = "";
+                        _gameManager.HumanController.TakeHealth(1);
+                    }
+                }
+                else {
+                    _passwordText = "";
+                    _gameManager.HumanController.TakeHealth(1);
+                }
+            }
+        }
+
