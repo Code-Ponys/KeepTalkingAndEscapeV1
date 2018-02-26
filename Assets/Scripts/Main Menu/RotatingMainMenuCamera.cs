19f685a7464d2da8b28f784eb1b8e30f60504c9a
Nekro9618 <bill9618halim@gmail.com>
2018-02-26 19:41:29 +0100

    Rotating Camera in Main Menu

---
+++ Assets/Scripts/Main Menu/RotatingMainMenuCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingMainMenuCamera : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(transform.position, transform.up, Time.deltaTime * 10f);
+	}
+}
