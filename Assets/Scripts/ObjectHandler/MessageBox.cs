046accdcac84c6df789d72d5bc2fc94cbc9ef055
CodeFoxByChojo <playhadde@gmail.com>
2018-02-27 17:17:26 +0100

    added MessageBox Script

---
+++ Assets/Scripts/ObjectHandler/MessageBox.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace TrustfallGames.KeepTalkingAndEscape.Listener {
+    public class MessageBox : MonoBehaviour {
+        [SerializeField] private float _playMessageAfterSeconds;
+        [SerializeField] private AudioClip _message;
+        [SerializeField] private SoundManager _soundManager;
+        private bool _played;
+
+
+        private void Start() {
+            _soundManager = SoundManager.GetSoundManager();
+        }
+
+        private void Update() {
+            if(_played) return;
+            if(_playMessageAfterSeconds < 0) {
+                _played = true;
+                _soundManager.Source.clip = _message;
+            }
+            else {
+                _playMessageAfterSeconds -= Time.deltaTime;
+            }
+        }
+    }
+}
